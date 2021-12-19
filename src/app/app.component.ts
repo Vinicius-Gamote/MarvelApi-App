5f9512f1ffcdd581c4897d61f7fa41bf9efd249d
Gamotera <viniciusgamote@gmail.com>
2021-12-19 18:47:21 -0300

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'MarvelApi';
+export class AppComponent implements OnInit{
+  title = 'Marvel Api'
+  public heroes: Observable<any> | undefined
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+
+    this.http.get("http://gateway.marvel.com/v1/public/characters?ts=1639846442&apikey=9183f800e30756ada325a557437ea8bb&hash=13beda4e1a0640c41aad4b4be8033df8")
+    .subscribe(d => console.log(d))
+
+    this.heroes = this.http.get("http://gateway.marvel.com/v1/public/characters?ts=1639846442&apikey=9183f800e30756ada325a557437ea8bb&hash=13beda4e1a0640c41aad4b4be8033df8")
+  }
 }
