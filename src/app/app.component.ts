7f23b7acadd2e9ee816e5ebca65e7ac20a8ef5a3
Gamotera <viniciusgamote@gmail.com>
2021-12-19 21:25:45 -0300

    Table with characters images, names and descriptions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,12 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit{
+
   title = 'Marvel Api'
   public heroes: Observable<any> | undefined
+  public showImg: boolean = false
+  public widthImg: Number = 150
+  public marginImg: Number = 2
 
   constructor(private http: HttpClient) { }
 
@@ -20,4 +24,9 @@ export class AppComponent implements OnInit{
 
     this.heroes = this.http.get("http://gateway.marvel.com/v1/public/characters?ts=1639846442&apikey=9183f800e30756ada325a557437ea8bb&hash=13beda4e1a0640c41aad4b4be8033df8")
   }
+
+  public alterImage(): void {
+    this.showImg = !this.showImg
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,9 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CollapseModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
